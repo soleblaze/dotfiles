68574f2e80296539ebb95e0932237a3f372c833f
Soleblaze <soleblaze@skyshadows.net>
2023-03-17 21:09:48 -0500

    Fix fzf

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,6 +1,6 @@
-if [ -f "/usr/share/fzf/key-bindings.zsh" ] ;then
-  source /usr/share/fzf/key-bindings.zsh
+if [ -f "$(brew --prefix fzf)/shell/key-bindings.zsh" ] ;then
+  source $(brew --prefix fzf)/shell/key-bindings.zsh
 fi
-if [ -f "/usr/share/fzf/completion.zsh" ] ;then
-  source /usr/share/fzf/completion.zsh
+if [ -f "$(brew --prefix fzf)/shell/completion.zsh" ] ;then
+  source $(brew --prefix fzf)/shell/completion.zsh
 fi
