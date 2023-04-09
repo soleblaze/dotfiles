43d58c850fed2d40ec4182ff63c89cf507995545
Soleblaze <soleblaze@skyshadows.net>
2023-04-08 23:06:11 -0500

    Add lightbulb

---
+++ nvim/lua/plugins/lightbulb.lua
@@ -0,0 +1,7 @@
+return {
+  'kosayoda/nvim-lightbulb',
+  dependencies = { 'antoinemadec/FixCursorHold.nvim' },
+  config = function()
+    require('nvim-lightbulb').setup({ autocmd = { enabled = true } })
+  end
+}
