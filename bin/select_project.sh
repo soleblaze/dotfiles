bfaa8710cdc59e1cdab6f7199833299ffa78d768
Soleblaze <soleblaze@skyshadows.net>
2023-04-10 23:23:56 -0500

    Create a timing project with new project

--- bin/select_project.sh
+++ bin/select_project.sh
@@ -6,8 +6,30 @@ if ! [ "$1" ]; then
   exit 1
 fi
 
+# Takes the basic size of the header and adds the length of the title to it
+calculate_length() {
+  local hex_number=$1
+  local decimal_number=${#TITLE}
+  local integer_hex_number=$((16#$hex_number))
+  local result=$((integer_hex_number + decimal_number))
+  printf "%02x" $result
+}
+
+# Generate a rule to match title strings
+generate_rule() {
+  LENGTH="$(calculate_length 00)"
+  LENGTH1="$(calculate_length 1D)"
+  LENGTH2="$(calculate_length 19)"
+  LENGTH3="$(calculate_length 17)"
+  LENGTH4="$(calculate_length 04)"
+  LENGTH5="$(calculate_length 02)"
+  STRINGHEX=$(echo -ne "${TITLE}" | xxd -p)
+  echo "0a${LENGTH1}080112${LENGTH2}12${LENGTH3}08631a0922070a057469746c6522${LENGTH4}0a${LENGTH5}0a${LENGTH}${STRINGHEX}2a020102" | xxd -r -p | base64
+}
+
 ROOTDIR="$HOME/projects"
 PROJECT="$1"
+TITLE="[$PROJECT]"
 
 if ! [[ -d "$ROOTDIR/$PROJECT" ]]; then
   if [[ "$PROJECT_REPO_BASE" ]]; then
@@ -50,6 +72,18 @@ if ! [[ -d "$ROOTDIR/$PROJECT" ]]; then
       jira issue view "${PROJECT}" >>"${note}"
     fi
   fi
+
+  # Create project
+  osascript <<EOD
+tell application "TimingHelper"
+	set results to projects whose name is "$PROJECT"
+	if results = {} then
+		set ParentProjects to (projects whose name is "projects")
+		set ParentProject to first item in ParentProjects
+		create project name "$PROJECT" parent project ParentProject productivity rating 1 rule data "$(generate_rule)"
+	end if
+end tell
+EOD
 fi
 
 smug start project PROJECT="${PROJECT}"
