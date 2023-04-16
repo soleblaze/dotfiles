29bb8e61cf45d9a28dd88cc36e2fb95e5a6a8f1c
Soleblaze <soleblaze@skyshadows.net>
2023-04-16 18:30:30 -0500

    update colima aliases to up resources on m1 ultra

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -56,7 +56,11 @@ function help() {
 
 # colima
 if [ "$(uname -m)" == "arm64" ]; then
-  alias cs='colima start --arch aarch64 --vm-type=vz --vz-rosetta --cpu 2 --memory 4'
+  if /Volumes/Macintosh\ HD/usr/sbin/system_profiler SPHardwareDataType | grep -q Ultra; then
+    alias cs='colima start --arch aarch64 --vm-type=vz --vz-rosetta --cpu 12 --memory 42'
+  else
+    alias cs='colima start --arch aarch64 --vm-type=vz --vz-rosetta --cpu 2 --memory 4'
+  fi
 else
   alias cs='colima start --cpu 2 --memory 4'
 fi
