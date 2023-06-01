2d4995fe9ed0e7b0a7ce939022f41d3cf3644524
Soleblaze <soleblaze@skyshadows.net>
2023-06-01 12:17:15 -0500

    Remove shellcheck code action
    I never use this and it causes a lot of notification scroll.

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -24,11 +24,8 @@ return {
       automatic_setup = true
     })
 
-    local h = require("null-ls.helpers")
-
     require("null-ls").setup({
       sources = {
-        null_ls.builtins.code_actions.shellcheck,
         null_ls.builtins.diagnostics.ansiblelint,
         null_ls.builtins.diagnostics.golangci_lint.with({
           args = {
