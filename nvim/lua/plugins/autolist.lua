0f3e4124449d3403d51800e0b79178b9c9b57b7b
Soleblaze <soleblaze@skyshadows.net>
2023-07-07 02:44:18 -0500

    Update autolist config

--- nvim/lua/plugins/autolist.lua
+++ nvim/lua/plugins/autolist.lua
@@ -5,18 +5,21 @@ return {
     "text",
     "tex",
     "plaintex",
+    "norg",
   },
   config = function()
-    local autolist = require("autolist")
-    autolist.setup()
-    autolist.create_mapping_hook("i", "<CR>", autolist.new)
-    autolist.create_mapping_hook("i", "<Tab>", autolist.indent)
-    autolist.create_mapping_hook("i", "<S-Tab>", autolist.indent, "<C-D>")
-    autolist.create_mapping_hook("n", "o", autolist.new)
-    autolist.create_mapping_hook("n", "O", autolist.new_before)
-    autolist.create_mapping_hook("n", ">>", autolist.indent)
-    autolist.create_mapping_hook("n", "<<", autolist.indent)
-    autolist.create_mapping_hook("n", "<C-r>", autolist.force_recalculate)
-    autolist.create_mapping_hook("n", "<leader>x", autolist.invert_entry, "")
+    require("autolist").setup()
+
+    vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
+    vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
+    vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
+    vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
+    vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
+    vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+    vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+    vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
+    vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
+    vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
+    vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
   end,
 }
