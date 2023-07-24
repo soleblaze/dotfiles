6b6c26875bb00bfe73250e69a5c72424a751eeee
Soleblaze <soleblaze@skyshadows.net>
2023-07-24 17:46:31 -0500

    Add lazynvim

---
+++ lazynvim/lua/config/lazy.lua
@@ -0,0 +1,38 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.test.core" },
+    { import = "plugins" },
+  },
+  defaults = {
+    lazy = false,
+    version = false,
+  },
+  checker = { enabled = true },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
---
+++ lazynvim/lua/plugins/dial.lua
@@ -0,0 +1,83 @@
+return {
+  "monaqa/dial.nvim",
+  config = function()
+    local augend = require("dial.augend")
+    require("dial.config").augends:register_group {
+      default = {
+        augend.integer.alias.decimal_int,
+        augend.integer.alias.hex,
+        augend.date.alias["%Y/%m/%d"],
+        augend.constant.new {
+          elements = { "and", "or" },
+          word = true,
+          cyclic = true,
+        },
+        augend.constant.new {
+          elements = { "&&", "||" },
+          word = false,
+          cyclic = true,
+        },
+        augend.constant.new {
+          elements = { ">", "<" },
+          word = false,
+          cyclic = true,
+        },
+        augend.constant.new {
+          elements = { "else", "elif" },
+          word = true,
+          cyclic = true,
+        },
+        augend.constant.new {
+          elements = { ">", "<" },
+          word = false,
+          cyclic = true,
+        },
+        augend.constant.new {
+          elements = { "on", "off" },
+          word = true,
+          cyclic = true,
+        },
+        augend.constant.new {
+          elements = { "yes", "no" },
+          word = true,
+          cyclic = true,
+        },
+        augend.constant.new {
+          elements = { ">", "<" },
+          word = false,
+          cyclic = true,
+        },
+        augend.constant.new {
+          elements = { "first", "last" },
+          word = true,
+          cyclic = true,
+        },
+        augend.constant.alias.bool,
+        augend.semver.alias.semver
+      },
+      visual = {
+        augend.integer.alias.decimal_int,
+        augend.integer.alias.hex,
+        augend.date.alias["%Y/%m/%d"],
+        augend.constant.alias.alpha,
+        augend.constant.alias.Alpha,
+        augend.semver.alias.semver,
+        augend.constant.new {
+          elements = { "-", "_" },
+          word = false,
+          cyclic = true,
+        }
+      },
+    }
+
+    -- change augends in VISUAL mode
+    vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal(), { noremap = true })
+    vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal(), { noremap = true })
+    vim.keymap.set("n", "g<C-a>", require("dial.map").inc_gnormal(), { noremap = true })
+    vim.keymap.set("n", "g<C-x>", require("dial.map").dec_gnormal(), { noremap = true })
+    vim.keymap.set("v", "<C-a>", require("dial.map").inc_visual(), { noremap = true })
+    vim.keymap.set("v", "<C-x>", require("dial.map").dec_visual(), { noremap = true })
+    vim.keymap.set("v", "g<C-a>", require("dial.map").inc_gvisual(), { noremap = true })
+    vim.keymap.set("v", "g<C-x>", require("dial.map").dec_gvisual(), { noremap = true })
+  end,
+}
