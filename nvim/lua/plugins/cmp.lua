59cf05014d321da94d4d12c3df3fce72a519b0c3
Soleblaze <soleblaze@skyshadows.net>
2023-07-28 21:51:39 -0500

    Remove buffer from cmp sources

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -15,6 +15,13 @@ return {
       local luasnip = require("luasnip")
       local cmp = require("cmp")
 
+      opts.sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "copilot", group_index = 2 },
+      })
+
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<C-n>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
