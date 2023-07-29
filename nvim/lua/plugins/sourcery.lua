6ea2d49f455d573b25c2398c85db6ebbd82862f6
Soleblaze <soleblaze@skyshadows.net>
2023-07-28 23:15:31 -0500

    Add sourcery

---
+++ nvim/lua/plugins/sourcery.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    {
+      "neovim/nvim-lspconfig",
+      dependencies = {
+        "mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "sourcery" })
+        end,
+      },
+      opts = {
+        servers = {
+          sourcery = {},
+        },
+      },
+      setup = {
+        sourcery = function(_, opts)
+        end,
+      },
+    },
+  }
+}
