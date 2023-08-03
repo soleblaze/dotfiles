e7db9c01d045b43062006f01a1d29fc46ae9e60f
Soleblaze <soleblaze@skyshadows.net>
2023-08-03 17:55:14 -0500

    Add diffview

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -1,6 +1,9 @@
 return {
   'NeogitOrg/neogit',
-  dependencies = 'nvim-lua/plenary.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    "sindrets/diffview.nvim"
+  },
   config = function()
     local neogit = require('neogit')
     neogit.setup({
