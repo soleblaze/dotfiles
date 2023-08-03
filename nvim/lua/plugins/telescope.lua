cc1fc96f9bf84651ebf4fde4f06f957eabff97f6
Soleblaze <soleblaze@skyshadows.net>
2023-08-03 17:52:37 -0500

    Add neogit

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -17,7 +17,9 @@ return {
     },
   },
   keys = {
-    { "<leader>fg", Util.telescope("live_grep"), desc = "Grep (root dir)" },
+    { "<leader>fg", Util.telescope("live_grep"),      desc = "Grep (root dir)" },
     { "<leader>/",  false },
+    { "<leader>gc", false },
+    { "<leader>gC", "<cmd>Telescope git_commits<CR>", desc = "commits" },
   }
 }
