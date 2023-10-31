3639db1104d4b72feb8b006383d5d2d7f0fc40e0
soleblaze <soleblaze@skyshadows.net>
2023-10-31 14:14:57 -0500

    Add treesj

---
+++ nvim/lua/plugins/treejs.lua
@@ -0,0 +1,9 @@
+return {
+  "Wansmer/treesj",
+  keys = {
+    { "gJ", "<cmd>TSJToggle<cr>", desc = "Join Line" },
+    { "gS", "<cmd>TSJSplit<cr>", desc = "Split Line" },
+    { "gM", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+  },
+  opts = { use_default_keymaps = false, max_join_length = 100 },
+}
