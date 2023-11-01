2743b51eb136e419271830a842d6ded3a68b53ff
soleblaze <soleblaze@skyshadows.net>
2023-10-31 21:23:48 -0500

    Move colima aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -62,16 +62,3 @@ function help() {
   "$@" --help 2>&1 | bathelp
 }
 
-# colima
-if which colima 2>&1 >/dev/null; then
-  if [ "$(uname -m)" == "arm64" ]; then
-    if /Volumes/Macintosh\ HD/usr/sbin/system_profiler SPHardwareDataType | grep -q Ultra; then
-      alias cs='colima start --arch aarch64 --vm-type=vz --vz-rosetta --cpu 12 --memory 42'
-    else
-      alias cs='colima start --arch aarch64 --vm-type=vz --vz-rosetta --cpu 2 --memory 4'
-    fi
-  else
-    alias cs='colima start --cpu 2 --memory 4'
-  fi
-  alias cstop='colima stop'
-fi
