cea45cf2e31da5d615f29c95a3f6618ef0465f2c
soleblaze <soleblaze@skyshadows.net>
2023-11-06 11:18:33 -0600

    Strip out comments that start with spaces

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,7 +2,7 @@
 
 # Strip comments and empty lines out of a file
 function catnc() {
-  grep -E -v '(^#|^/)' "$1" | sed '/^$/d' | bat -p --pager=never
+  grep -E -v '(^#|^/|^\s+#)' "$1" | sed '/^$/d' | bat -p --pager=never
 }
 
 function diff() {
