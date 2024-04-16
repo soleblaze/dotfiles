71a36612ed0a24b76ceda340e65a3e5e272a298b
soleblaze <soleblaze@skyshadows.net>
2024-04-15 22:36:24 -0500

    Update lualine setup

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,66 @@
+local icons = require("lazyvim.config").icons
+
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  opts = {
+    options = {
+      section_separators = "",
+      component_separators = "",
+    },
+    sections = {
+      lualine_a = {
+        {
+          "mode",
+          fmt = function(str)
+            return str:sub(1, 1)
+          end,
+        },
+      },
+      lualine_c = {
+        { LazyVim.lualine.pretty_path() },
+        {
+          "diff",
+          symbols = {
+            added = icons.git.added,
+            modified = icons.git.modified,
+            removed = icons.git.removed,
+          },
+          source = function()
+            local gitsigns = vim.b.gitsigns_status_dict
+            if gitsigns then
+              return {
+                added = gitsigns.added,
+                modified = gitsigns.changed,
+                removed = gitsigns.removed,
+              }
+            end
+          end,
+        },
+      },
+      lualine_x = {
+        {
+          function()
+            return "  " .. require("dap").status()
+          end,
+          cond = function()
+            return package.loaded["dap"] and require("dap").status() ~= ""
+          end,
+          color = LazyVim.ui.fg("Debug"),
+        },
+        {
+          "diagnostics",
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
+        },
+        "filetype",
+      },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
+  },
+}
