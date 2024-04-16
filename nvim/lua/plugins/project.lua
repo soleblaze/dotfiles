fef044d039a7a2f587c8775ce96eaa50b7d5dddc
soleblaze <soleblaze@skyshadows.net>
2024-04-16 12:13:11 -0500

    Exclude libraries from project list

--- nvim/lua/plugins/project.lua
+++ nvim/lua/plugins/project.lua
@@ -3,6 +3,13 @@ return {
     "ahmedkhalf/project.nvim",
     opts = {
       manual_mode = false,
+      exclude_dirs = {
+        "/opt/homebrew/*",
+        "~/.cargo/*",
+        "~/.gem/*",
+        "~/.local/*",
+        "~/.rbenv/*",
+      },
     },
   },
 }
