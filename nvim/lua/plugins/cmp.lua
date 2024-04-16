f8fa2cfceb9deb371cce10df6ff709978873609e
soleblaze <soleblaze@skyshadows.net>
2024-04-16 09:33:09 -0500

    Cleanup cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -3,14 +3,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-emoji",
   },
-  ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
     local luasnip = require("luasnip")
     local cmp = require("cmp")
 
@@ -30,7 +23,7 @@ return {
         else
           fallback()
         end
-      end, { "i", "s", }),
+      end, { "i", "s" }),
       ["<Tab>"] = cmp.mapping(function()
         if luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
@@ -40,14 +33,14 @@ return {
             select = false,
           })
         end
-      end, { "i", "s", }),
+      end, { "i", "s" }),
       ["<S-Tab>"] = cmp.mapping(function(fallback)
         if luasnip.jumpable(-1) then
           luasnip.jump(-1)
         else
           fallback()
         end
-      end, { "i", "s", }),
+      end, { "i", "s" }),
       ["<C-n>"] = cmp.mapping(function(fallback)
         if luasnip.jumpable(1) then
           luasnip.jump(1)
@@ -56,7 +49,7 @@ return {
         else
           fallback()
         end
-      end, { "i", "s", }),
+      end, { "i", "s" }),
       ["<C-p>"] = cmp.mapping(function(fallback)
         if luasnip.jumpable(-1) then
           luasnip.jump(-1)
@@ -65,21 +58,21 @@ return {
         else
           fallback()
         end
-      end, { "i", "s", }),
+      end, { "i", "s" }),
       ["<Down>"] = cmp.mapping(function(fallback)
         if cmp and cmp.visible() then
           cmp.select_next_item()
         else
           fallback()
         end
-      end, { "i", "s", }),
+      end, { "i", "s" }),
       ["<Up>"] = cmp.mapping(function(fallback)
         if cmp and cmp.visible() then
           cmp.select_prev_item()
         else
           fallback()
         end
-      end, { "i", "s", }),
+      end, { "i", "s" }),
       ["<CR>"] = cmp.mapping(function()
         cmp.autopairs.on_confirm_done({ map_char = { tex = "" } })
       end, {}),
