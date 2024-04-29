0a329e643fe9b1981c117cf0d7e6ea6d3ad89b0b
soleblaze <soleblaze@skyshadows.net>
2024-04-29 13:54:29 -0500

    Remove vimleave autocommand
    This causes a crash per: https://github.com/neovim/neovim/issues/21856

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -9,23 +9,12 @@ local function rename_tmux_window()
   end
 end
 
-local function restore_tmux_window_name()
-  local tmux_rename_cmd = "tmux setw automatic-rename"
-  vim.fn.system(tmux_rename_cmd)
-end
-
 vim.api.nvim_create_augroup("tmux", {})
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "FileReadPost" }, {
   group = "tmux",
   pattern = "*",
   callback = rename_tmux_window,
-})
-
-vim.api.nvim_create_autocmd("VimLeave", {
-  group = "tmux",
-  pattern = "*",
-  callback = restore_tmux_window_name,
 })
 
 return {}
