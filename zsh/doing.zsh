6be7cbdb082657944d6710c9f5a981bd412879fc
soleblaze <soleblaze@skyshadows.net>
2024-05-10 16:34:01 -0500

    Rename doing aliases

--- zsh/doing.zsh
+++ zsh/doing.zsh
@@ -3,10 +3,10 @@
 # Doing Aliases
 # https://github.com/ttscoff/doing
 if  [ $commands[doing] ]; then
-  alias dn="doing now"
   alias ddone="doing done"
   alias dl="doing last"
-  alias dnext="doing done; doing now"
+  alias dn="doing done; doing now"
+  alias dnow="doing now"
   alias dt="doing today --totals"
   alias dy="doing yesterday --totals"
 fi
