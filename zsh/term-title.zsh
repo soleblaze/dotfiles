7c40221114345d6bcc4f419691d6cf719de2deb3
soleblaze <soleblaze@skyshadows.net>
2024-05-10 16:29:09 -0500

    Fix term title

--- zsh/term-title.zsh
+++ zsh/term-title.zsh
@@ -4,14 +4,6 @@ typeset -a excluded_commands
 excluded_commands=('ls' 'cd' 'echo' 'pwd', 'git', 'cat', 'bat')
 
 function title-precmd() {
-
-  if  git rev-parse --show-toplevel >/dev/null 2>&1; then
-    GIT_BASE=$(basename $(git rev-parse --show-toplevel))
-    print -Pn "\e]2;GIT - ${(q)GIT_BASE}\a"
-  else
-    print -Pn "\e]2;${PWD/#$HOME/~}\a"
-  fi
-
   if [ "$TMUX" ]; then
     if [[ "$PWD" == "$HOME" ]]; then
       print -n "\ekz:~\e\\"
@@ -32,7 +24,7 @@ function title-preexec() {
 
 autoload -Uz add-zsh-hook
 
-if [[ "$TERM" == (foot*|kitty*|screen*|tmux*|xterm*) ]]; then
+if [[ "$TERM" == "tmux-256color" ]]; then
   add-zsh-hook -Uz precmd title-precmd
   add-zsh-hook -Uz preexec title-preexec
 fi
